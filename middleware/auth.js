18056d4087e0293c58d448244045cd715eb07298
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-12 21:33:19 +0100

    Middleware Auth

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const {
+    User
+} = require('../models');
+
+const auth = async(req,res,next)=>{
+    try {
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, 'noclones');
+        const user = await User.findByPk(payload.id);
+        if(!user){
+            return res.status(401).send({
+                message: 'You are not allowed'
+            })
+        }
+        req.user = user;
+        next()
+    } catch (error) {
+        console.error(error)
+        return res.status(401).send({
+            message: 'You are not allowed',error
+        })
+    }
+}
+
+module.exports = auth;
