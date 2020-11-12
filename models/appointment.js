1a0a3169a04ccd0b972997bc4a3f1f6f7b0b68f7
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-12 21:34:39 +0100

    models appointment updated

---
+++ models/appointment.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Appointment extends Model {
+    static associate(models) {
+      this.belongsTo(models.User)
+      this.belongsTo(models.User, {
+        as: 'Dtr',
+        foreignKey: 'doctorId'
+      })
+    }
+  };
+  Appointment.init({
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }, 
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: null
+    },
+    doctorId: {
+      type: DataTypes.INTEGER,
+      allowNull: null
+    },
+    allergies: DataTypes.BOOLEAN,
+    covid: DataTypes.BOOLEAN,
+    description: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Appointment',
+  });
+  return Appointment;
+};
