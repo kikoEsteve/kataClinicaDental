57f6915fdb662b292cc45e323b83ba807fdb8ef2
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-17 00:25:01 +0100

    routes/users updated_2.0

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,12 @@ const UserController = require('../controllers/UserController');
 
 //Route to show all users
 router.get('/', UserController.getAll);
-router.post('/signup', function(req,res){
-    UserController.signup });
+router.post('/signup', UserController.signup);
 
 router.post('/login', UserController.login);
 
-router.put('/updateUser/:id', function(req,res){
-    UserController.update }); 
-router.delete('/user/:id', function(req,res) {
-    UserController.delete }); 
+// router.put('/update/:id', UserController.update); 
+// router.delete('/user/:id', UserController.delete); 
 
 
 module.exports = router;
