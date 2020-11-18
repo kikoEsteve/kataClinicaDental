8f37bf8e278060fa9237998aa1c2bf12bbab6795
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-18 16:44:05 +0100

    Added process.env.PORT en app.js_b

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const appointmentsRouter = require('./routes/appointments');
 const usersRouter = require('./routes/users');
 const app = express();
-const port = proces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 //Evitamos undefined en el req.body
 app.use(express.json());
