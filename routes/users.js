a200a4f302b698049cc72913b56e56cdf9875b21
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-18 10:52:02 +0100

    added start and serve at scripts in package.json

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const UserController = require('../controllers/UserController');
 
 //Route to show all users
-router.get('/', UserController.getAll);
-router.post('/signup', UserController.signup);
+router.get('./', UserController.getAll);
+router.post('./signup', UserController.signup);
 
-router.post('/login', UserController.login);
+router.post('./login', UserController.login);
 
 // router.put('/update/:id', UserController.update); 
 // router.delete('/user/:id', UserController.delete); 
