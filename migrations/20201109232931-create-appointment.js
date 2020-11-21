a7c24452e38aff615be1ffbe9999e78ec6bf0573
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-21 16:40:02 +0100

    autoincrement appointments to false

--- migrations/20201109232931-create-appointment.js
+++ migrations/20201109232931-create-appointment.js
@@ -4,7 +4,7 @@ module.exports = {
     await queryInterface.createTable('Appointments', {
       id: {
         allowNull: false,
-        autoIncrement: true,
+        autoIncrement: false,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
